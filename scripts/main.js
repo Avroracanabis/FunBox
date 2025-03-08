2f9e2cf07f689fcb6a8545014bbfe24b51df5ee4
Avroracanabis <abduazimdiu@gmail.com>
2025-03-09 04:38:32 +0500

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Авторизация через Google
+document.getElementById('google-auth').addEventListener('click', () => {
+    alert("Авторизация через Google");
+});
+
+// Авторизация через ВКонтакте
+document.getElementById('vk-auth').addEventListener('click', () => {
+    alert("Авторизация через ВКонтакте");
+});
