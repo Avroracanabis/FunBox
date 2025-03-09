2aaf7b9379c1e1ece79907e69ae5e938a20acf4d
Avroracanabis <abduazimdiu@gmail.com>
2025-03-09 06:06:14 +0500

    Delete styles directory

--- styles/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Пример обработки кнопок
-document.getElementById('login').addEventListener('click', () => {
-    alert("Функция входа в разработке!");
-});
-
-document.getElementById('register').addEventListener('click', () => {
-    alert("Функция регистрации в разработке!");
-});
