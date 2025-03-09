4780835e24365ba828d25b0afc77a1fb291a2b89
Avroracanabis <abduazimdiu@gmail.com>
2025-03-09 06:16:23 +0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Добро пожаловать на FunZic!");
-
-    const form = document.getElementById("contact-form");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("Спасибо за ваше сообщение, " + document.getElementById("name").value + "!");
-    });
-});
