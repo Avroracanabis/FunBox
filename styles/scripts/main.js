5b6e27e383cce9e8ea78fe79a2de8e7b039d13d0
Avroracanabis <abduazimdiu@gmail.com>
2025-03-09 05:04:33 +0500

    Create main.js

---
+++ styles/scripts/main.js
@@ -0,0 +1,8 @@
+// Пример обработки кнопок
+document.getElementById('login').addEventListener('click', () => {
+    alert("Функция входа в разработке!");
+});
+
+document.getElementById('register').addEventListener('click', () => {
+    alert("Функция регистрации в разработке!");
+});
